1ceb698d0417fb116ddb85043c8470f59ae3eca5
mayurgarg <gargmayur2025@gmail.com>
2025-08-31 16:07:49 +0530

    route topic

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ app.get("/", (req, res) => {
   res.send("this is a response");
 });
 
+//simple route
 app.get("/home", (req, res) => {
   res.send("this is a home response");
+});
+
+// nested route
+app.get("/home/user", (req, res) => {
+  res.send("this is a usr response");
+});
+
+//pass value through Route Parameters
+app.get("/home/user/:userid", (req, res) => {
+  res.send(req.params.userid);
+});
+
+app.get("/search", (req, res) => {
+  res.send(req.query);
 });
